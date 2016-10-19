896904aaee053725c1c588c4181d80f041ce8cf0
Marek Kochanowski <marek@kochanow.ski>
2016-10-19 21:30:01 +0200

    Fix tabs, finally! Still no API support, tho

--- IonicApp/src/pages/search/search.ts
+++ IonicApp/src/pages/search/search.ts
@@ -1,22 +1,15 @@
 import { Component } from '@angular/core';
+
 import { NavController } from 'ionic-angular';
 
-/*
-  Generated class for the Search page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-search',
   templateUrl: 'search.html'
 })
-export class Search {
+export class SearchPage {
 
-  constructor(public navCtrl: NavController) {}
-
-  ionViewDidLoad() {
-    console.log('Hello Search Page');
+  constructor(public navCtrl: NavController) {
+    
   }
 
 }
--- IonicApp/src/pages/tabs/tabs.ts
+++ IonicApp/src/pages/tabs/tabs.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { TopPage } from '../top/top';
+import { HomePage } from '../home/home';
 import { AboutPage } from '../about/about';
 import { SearchPage } from '../search/search';
 
@@ -10,7 +10,7 @@ import { SearchPage } from '../search/search';
 export class TabsPage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
-  tab1Root: any = TopPage;
+  tab1Root: any = HomePage;
   tab2Root: any = SearchPage;
   tab3Root: any = AboutPage;
 
